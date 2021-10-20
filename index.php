29c9d182899387feebfc14c9732f6c49fd856ea7
IlarioCOCCO <ilario.cocco@gmail.com>
2021-10-20 17:31:46 +0200

    debag css

--- index.php
+++ index.php
@@ -21,33 +21,37 @@
 <body>
 
     <header>
-        <div class="logo">
-            <img src="./img/googlelogo_clr_74x24px.svg" alt="logo google">
+        <div class="boxHeader">
+            <div class="logo">
+                <img src="./img/googlelogo_clr_74x24px.svg" alt="logo google">
+                <h3 class="privacy">Privacy e termini</h3>
+            </div>
+            <div>
+                <ul class="listato">
+                    <li>Introduzione</li>
+                    <li>Norme sulla praivacy</li>
+                    <li>Termini di servizio</li>
+                    <li>Tecnologie</li>
+                    <li>Domande frequenti.</li>
+                </ul>
+                <hr>
+            </div>
         </div>
-        <div>
-            <ul class="listato">
-                <li>Introduzione</li>
-                <li>Norme sulla praivacy</li>
-                <li>Termini di servizio</li>
-                <li>Tecnologie</li>
-                <li>Domande frequenti.</li>
-            </ul>
-            <hr>
-        </div>
-    </header>
-    <main>
-        <container>
-            <box class="page">
-                <div class="">
-                    <h2>ciao</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Autem, quos praesentium expedita quaerat quas necessitatibus veniam voluptates aperiam.
-                        Deleniti architecto ut iure aut iste minus, quia optio adipisci dolorum porro!
-                    </p>
-                </div>
 
-            </box>
-        </container>
+    </header>
+
+    <main>
+
+        <box>
+            <div class="page">
+                <h2>ciao</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Autem, quos praesentium expedita quaerat quas necessitatibus veniam voluptates aperiam.
+                    Deleniti architecto ut iure aut iste minus, quia optio adipisci dolorum porro!
+                </p>
+            </div>
+        </box>
+       
     </main>
     <footer></footer>
 
