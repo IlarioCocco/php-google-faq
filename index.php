9485c9eb05fdce43da91b0dd005b05ce1f3f1f4d
IlarioCOCCO <ilario.cocco@gmail.com>
2021-10-20 17:11:49 +0200

    debug start esercizio reset

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <header>
+        <div class="logo">
+            <img src="./img/googlelogo_clr_74x24px.svg" alt="logo google">
+        </div>
+        <div>
+            <ul class="listato">
+                <li>Introduzione</li>
+                <li>Norme sulla praivacy</li>
+                <li>Termini di servizio</li>
+                <li>Tecnologie</li>
+                <li>Domande frequenti.</li>
+            </ul>
+            <hr>
+        </div>
+    </header>
+    <main>
+        <container>
+            <box class="page">
+                <div class="">
+                    <h2>ciao</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Autem, quos praesentium expedita quaerat quas necessitatibus veniam voluptates aperiam.
+                        Deleniti architecto ut iure aut iste minus, quia optio adipisci dolorum porro!
+                    </p>
+                </div>
+
+            </box>
+        </container>
+    </main>
+    <footer></footer>
+
+</body>
+
+</html>
